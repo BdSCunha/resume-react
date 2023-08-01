332062e285157719fcc3e607bda8edce3474d702
Bruno da Silva Cunha <bruno.cunha2@external.stellantis.com>
2023-08-01 13:43:59 -0300

    Closes #4 - Create the `Footer` component

--- resume-react/src/components/Main.tsx
+++ resume-react/src/components/Main.tsx
@@ -3,7 +3,9 @@ import styled from 'styled-components';
 
 const StyledMain = styled.main`
     flex: 1;
+    width: 100%;
     display: flex;
+    flex-direction: column;
     align-items: center;
     justify-content: center;
 `;
